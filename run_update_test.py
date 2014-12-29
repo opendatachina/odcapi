2d7dfb4df779298bd8526bf946772d00a5368c20
Andrew Hyder <hyd415@gmail.com>
2014-12-29 12:15:59 -0800

    fix from issue updating

--- run_update_test.py
+++ run_update_test.py
@@ -103,10 +103,10 @@ class RunUpdateTestCase(unittest.TestCase):
                 for proj_info in projects:
                     run_update.save_project_info(self.db.session, proj_info)
 
-                issues, labels = run_update.get_issues(organization.name)
+                issues = run_update.get_issues(organization.name)
 
-                for i in range(0, len(issues)):
-                    run_update.save_issue_info(self.db.session, issues[i], labels[i])
+                for issue in issues:
+                    run_update.save_issue(self.db.session, issue)
 
         self.db.session.flush()
 
