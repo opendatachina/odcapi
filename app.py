7bc9fd32987ef689ccfda3c1479b8265ddd0cda3
Thomas Apodaca <tma@angrymonkey.com>
2015-02-04 13:24:48 -0800

    handling 404 & 500 errors

--- app.py
+++ app.py
@@ -1021,5 +1021,13 @@ def api_static_file(path):
     response.headers['Content-Type'] = mime_type
     return response
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return jsonify({"status":"Resource Not Found"}), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return jsonify({"status":"Resource Not Found"}), 500
+
 if __name__ == "__main__":
     manager.run()
